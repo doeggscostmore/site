81a99e719d59ae1794518e92de5bd24ceee2d038
DoEggsCostMore <>
2024-11-11 15:22:20 -0600

    Add  rel="noopener noreferrer" to footer links

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -14,7 +14,7 @@
         <div class="overlay"></div>
         <footer>
             &copy; 2024. | 
-            Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a> |
+            Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" rel="noopener noreferrer">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"  rel="noopener noreferrer">Unsplash</a> |
             <a href="/about">About</a>
         </footer>
     </div>
