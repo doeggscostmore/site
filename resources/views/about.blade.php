295fb93bae191b3f1395870a9a5211d332b53b6e
DoEggsCostMore <>
2024-11-11 22:53:19 -0600

    Update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Do Eggs Cost More?')
+@section('title', 'About | Do Eggs Cost More?')
 
 @section('content')
     <div class="page">
@@ -20,50 +20,18 @@
                     <p>
                         Do Eggs Cost More is a slightly political site that
                         tracks the prices of common grocery items, and compares
-                        them to January 20, 2025. While political in nature,
-                        the data presented is not skewed or modified to make a
-                        point (if I'm wrong, I'm wrong).
+                        them to various key dates after the 2024 election. While
+                        political in nature, the data presented is not skewed or
+                        modified to make a point (if I'm wrong, I'm wrong).
                     </p>
                     <p>
                         I drew inspiration largely from others who vowed to
-                        track grocery prices manually after the 2024 election.
-                        Since some of this data is available on free APIs, I
-                        made use of those and created this cheeky website to
-                        summarize what we all really want to know: do eggs cost
-                        more than they did.
+                        track grocery prices manually after arguments about
+                        grocery prices were made. Since some of this data is
+                        available on free APIs, I made use of those and created
+                        this cheeky website to summarize what we all really want
+                        to know: do eggs cost more than they did.
                     </p>
-
-                    <h2>Methodolgy</h2>
-                    <p>
-                        The data is collected twice a day from 10 grocery store
-                        locations that belong to the same chain. The locations
-                        are spread across their operating area.
-                    </p>
-                    <p>
-                        For each category of item (eggs, milk, etc), we're
-                        getting at least 3 specific products and their prices
-                        from the grocery store. These items are not selected in
-                        any particular way, but they include a mix of variaties,
-                        brands, sizes, etc. More are added periodically, but
-                        around 40 specific items are tracked at the start.
-                    </p>
-                    <p>
-                        I'm not sharing the specific store or products for two
-                        reasons:
-                    </p>
-                    <ul>
-                        <li>
-                            The specific products and stores are immiterial to
-                            the point I'm making. This isn't a tracker for a
-                            specific price of eggs at a specific store, but a
-                            general look at the cost of eggs.
-                        </li>
-                        <li>
-                            The grocery store may or may not be OK with my use
-                            of the API in this way. They don't say I can't, so I
-                            guess that means I can?
-                        </li>
-                    </ul>
 
                     <h2>Contact</h2>
                     <p>
@@ -85,11 +53,13 @@
                         rel="noopener noreferrer">on GitHub.</a>
                     </p>
                     <p>
-                        I don't modify the data I get and the caluclations are
+                        I don't modify the data I get and the calculations are
                         accurate to the best of my knowledge, but I'm not a
                         statistician. Please don't form policy or make life
                         altering decisions based on the data I present on this
-                        site.
+                        site.  More information about the methods used to
+                        collect and calculate data are on the <a
+                        href="/methodology">Methodology Page</a>.
                     </p>
                     <p>
                         This site does not use cookies, but we may collect some
@@ -98,7 +68,7 @@
                     <p>
                         This site is not funded by, endorsed by, or affiliated
                         with any policial party, politician or political action
-                        comittee; any NGOs; orange cats; or strange men who
+                        committee; any NGOs; orange cats; or strange men who
                         would describe themselves as chimney enthusiasts.
                     </p>
                 </div>
@@ -107,7 +77,7 @@
         <div class="container">
             <div class="row">
                 <footer>
-                    &copy; 2024. | 
+                    &copy; 2024. | <a href="/methodology">Methodology<a> <br />
                     Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" rel="noopener noreferrer">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"  rel="noopener noreferrer">Unsplash</a>
                 </footer>
             </div>
