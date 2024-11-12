5c97c7da5e1e9a26dee38dae70416c6d55459eeb
DoEggsCostMore <>
2024-11-11 22:58:04 -0600

    Add methodologies page

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/check/{id}', [ProductController::class, 'product']);
-Route::get('/', [ProductController::class, 'home']);
-
 Route::get('/about', function () {
     return response()->view('about');
 });
+Route::get('/methodology', function () {
+    return response()->view('methodology');
+});
+
+Route::get('/{id}', [ProductController::class, 'product']);
+Route::get('/', [ProductController::class, 'home']);
