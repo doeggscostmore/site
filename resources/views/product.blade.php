895e82acfbfa665de891e33bd74eea9f91e7c1b8
DoEggsCostMore <>
2024-11-13 00:40:53 -0600

    Fix round vs number_format

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -46,16 +46,16 @@
                         @if ($event->name != '2024 Presidential Election')
                             <div class="col-md-3 event-change">
                                 @if ($event->isUp)
-                                    <span class="up">${{ round($event->price, 2) }}</span>
+                                    <span class="up">${{ number_format($event->price, 2) }}</span>
                                     <span class="small">Up {{ round($event->change, 2) }}%</span>
                                 @else
-                                    <span class="down">${{ round($event->price, 2) }}</span>
+                                    <span class="down">${{ number_format($event->price, 2) }}</span>
                                     <span class="small">Down {{ round($event->change, 2) }}%</span>
                                 @endif
                             </div>
                         @else
                             <div class="col-md-3 event-change">
-                                <span class="baseline">${{ round($event->price, 2) }}</span>
+                                <span class="baseline">${{ number_format($event->price, 2) }}</span>
                             </div>
                         @endif
                     </div>
