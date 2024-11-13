7b28b9b9737de2f53f68fbffd5bcfed154a438d3
DoEggsCostMore <>
2024-11-12 21:42:04 -0600

    Update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,46 @@
 <footer>
-    &copy; 2024. | 
-    Photo by <a href="https://unsplash.com/@helloimnik?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" rel="noopener noreferrer">Nik</a> on <a href="https://unsplash.com/photos/brown-eggs-in-a-box-LUYD2b7MNrg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"  rel="noopener noreferrer">Unsplash</a> |
-    <a href="/about">About</a>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                &copy; {{ date('Y') }}
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <ul role="navigation" class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/methodology">Methodology</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="/about#trackingoptout">Do Not Sell My Information</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container seo">
+        <div class="row">
+            <div class="col">
+                <p>
+                    Do Eggs Cost More tracks the prices of grocery items over
+                    time and compares them to past times and certain political
+                    events.
+                </p>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <ul role="navigation" class="nav justify-content-center">
+                        @foreach ($categories as $category)
+                        <li class="nav-item">
+                            <a href="/{{ $category->slug }}">{{ ucwords($category->name) }} Prices</a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
