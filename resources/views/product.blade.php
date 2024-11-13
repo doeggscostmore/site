ee43c67308e342e772b8052a862c26d4ba128a0d
DoEggsCostMore <>
2024-11-12 20:29:17 -0600

    Update share links

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -69,23 +69,23 @@
                 </div>
                 <div class="row text-center">
                     <div class="col">
-                        <a href="https://www.facebook.com/sharer.php?u={{ $canonical }}">
+                        <a href="https://www.facebook.com/sharer.php?u={{ $canonical }}" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-facebook-f"></i>
                             <span class="sr-only">Share to Facebook</span>
                         </a>
-                        <a href="https://twitter.com/share?url={{ $canonical }}">
+                        <a href="https://twitter.com/share?url={{ $canonical }}" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-twitter"></i>
                             <span class="sr-only">Share to Twitter</span>
                         </a>
-                        <a href="https://reddit.com/submit?url={{ $canonical }}">
+                        <a href="https://reddit.com/submit?url={{ $canonical }}" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-reddit"></i>
                             <span class="sr-only">Share to Reddit</span>
                         </a>
-                        <a href="https://www.threads.net/intent/post?text={{ urlencode($canonical) }}">
+                        <a href="https://www.threads.net/intent/post?text={{ urlencode($canonical) }}" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-threads"></i>
                             <span class="sr-only">Share to Threads</span>
                         </a>
-                        <a href="https://bsky.app/intent/post?text={{ urlencode($canonical) }}">
+                        <a href="https://bsky.app/intent/post?text={{ urlencode($canonical) }}" target="_blank" rel="noopener noreferrer">
                             <i class="fab fa-bluesky"></i>
                             <span class="sr-only">Share to Bluesky</span>
                         </a>
