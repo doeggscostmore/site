87a14f4608f36c5618abc349b844a29918f2c2dd
DoEggsCostMore <>
2024-11-15 14:08:44 -0600

    Update license format because it bothered me

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import VitePluginWebpCompress from 'vite-plugin-webp-compress';
 
 export default defineConfig({
+    esbuild: { legalComments: 'eof' },
+
     plugins: [
         laravel({
             input: [
                 // SCSS
-                'resources/css/app.scss', 
+                'resources/css/app.scss',
 
                 // JS
                 'resources/js/app.js',
