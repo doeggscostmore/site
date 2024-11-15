bab69530a726c94217c556fdd8ed9be6a94f0e6d
DoEggsCostMore <>
2024-11-15 10:33:24 -0600

    Make curl silent

--- purge.sh
+++ purge.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" -H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json" --data '{"purge_everything":true}'
+curl -s -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" -H "X-Auth-Email: $CF_EMAIL" -H "X-Auth-Key: $CF_API_KEY" -H "Content-Type: application/json" --data '{"purge_everything":true}'
