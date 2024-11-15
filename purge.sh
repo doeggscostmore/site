b7699755eb3f8a3924fdffeee995fe2b18937814
DoEggsCostMore <>
2024-11-15 10:04:32 -0600

    Add a step to purge the CDN at the end of the deploy

---
+++ purge.sh
@@ -0,0 +1,1 @@
+curl -X POST "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache" -H "Authorization: Bearer {$CF_API_KEY}" -H "Content-Type: application/json" --data '{"purge_everything":true}'
