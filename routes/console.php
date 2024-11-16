e23d9a27e3ad06d8d01e50b3820d1adacbff9cd2
DoEggsCostMore <>
2024-11-16 01:39:52 -0600

    Add EIA to schedule of things

--- routes/console.php
+++ routes/console.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Console\Commands\GetPrices;
+use App\Console\Commands\GetPricesEia;
 use App\Console\Commands\GetPricesKroger;
 use App\Console\Commands\WarmCache;
 use Illuminate\Foundation\Inspiring;
@@ -8,4 +9,5 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command(GetPricesKroger::class)->twiceDaily();
+Schedule::command(GetPricesEia::class)->weeklyOn('tuesday');
 Schedule::command('export')->hourlyAt(30);
