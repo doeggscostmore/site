096c640c6de31d761836a22631bb0ea5ddac4ccc
DoEggsCostMore <>
2024-11-16 22:43:55 -0600

    Fix price migration

--- database/migrations/2024_11_16_205958_remove_price_time.php
+++ database/migrations/2024_11_16_205958_remove_price_time.php
@@ -3,6 +3,7 @@
 use App\Models\Price;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,7 +19,17 @@ return new class extends Migration
 
         // Loop through and remove data from the same product, same location and
         // same day.
+        $saved = [];
         foreach (Price::all() as $price) {
+            // This has a side effect of deleting all the rows in the table
+            // (mostly) since we don't run a new ::all after each delete.  This
+            // picks the first row we come across for this unique constraint and
+            // saves it.
+            if (!in_array($price->product_id . $price->location_id . $price->time, $saved)) {
+                $saved[] = $price->product_id . $price->location_id . $price->time;
+                continue;
+            }
+
             Price::where('product_id', $price->product_id)
                 ->where('location_id', $price->location_id)
                 ->where('time', $price->time)
