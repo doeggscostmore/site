d3ae0d097cc1444437b4dafc04150d0890d7a31a
DoEggsCostMore <>
2024-11-17 15:43:42 -0600

    Update seeders to just import Raw SQL

---
+++ database/seeders/SqlSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SqlSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::unprepared(
+            file_get_contents(__DIR__ . '/../../seeds/app.sql')
+        );
+    }
+}
