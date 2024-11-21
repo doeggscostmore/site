efc74b2a6798bc7f5a04e04fbeaf9a8cb8d2ebf0
DoEggsCostMore <>
2024-11-21 00:58:40 -0600

    Remove unused code

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategorySeeder::class,
-            ProductSeeder::class,
-            LocationsSeeder::class,
             PriceSeeder::class,
-            EventSeeder::class,
         ]);
     }
 }
