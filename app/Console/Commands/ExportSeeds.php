1f8bfbdd3b7f6081c7d96574d31d9fd154c6bc71
DoEggsCostMore <>
2024-11-21 01:02:05 -0600

    Update export seeds, export seeds

--- app/Console/Commands/ExportSeeds.php
+++ app/Console/Commands/ExportSeeds.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\BlsPrice;
 use App\Models\Event;
 use App\Models\Price;
 use App\Models\Product;
@@ -30,10 +31,6 @@ class ExportSeeds extends Command
      */
     public function handle()
     {
-        file_put_contents('./seeds/products.php', var_export(Product::all()->toArray(), true));
-        file_put_contents('./seeds/prices.php', var_export(Price::all()->toArray(), true));
-        file_put_contents('./seeds/categories.php', var_export(ProductCategory::all()->toArray(), true));
-        file_put_contents('./seeds/locations.php', var_export(StoreLocation::all()->toArray(), true));
-        file_put_contents('./seeds/events.php', var_export(Event::all()->toArray(), true));
+        file_put_contents('./seeds/prices.php', var_export(BlsPrice::all()->toArray(), true));
     }
 }
