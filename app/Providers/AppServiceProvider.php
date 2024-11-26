b4c5566621f899cc5a9f6412733103cd6406e22b
DoEggsCostMore <>
2024-11-26 00:21:32 -0600

    Better fix cor the app service provider issues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Data;
+use Exception;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::share('categories', Data::Categories());
+        // Depending on the state of the environment, this might fail.  If it
+        // does, we just ignore it. This will still throw an error, which is
+        // what we want.
+        try {
+            View::share('categories', Data::Categories());
+        } catch (Exception $e) {
+            //
+        }
     }
 }
