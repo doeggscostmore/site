c4da2a49f1e663e5857220c4ee1fd2a559fdb035
DoEggsCostMore <>
2025-01-22 23:01:00 -0600

    Run the reddit bot every minute

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,7 @@ use App\Console\Commands\GetData;
 use App\Console\Commands\GetPrices;
 use App\Console\Commands\GetPricesEia;
 use App\Console\Commands\GetPricesKroger;
+use App\Console\Commands\RedditBot;
 use App\Console\Commands\WarmCache;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
@@ -13,4 +14,5 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command(GetData::class)->weekly('18:00');
 Schedule::command(GenerateSitemap::class, ['storage' => 'public'])->weekly('20:00');
 
-// Schedule::command('export')->daily();
+// Run the bots every minute
+Schedule::command(RedditBot::class)->everyMinute();
