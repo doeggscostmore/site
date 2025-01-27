acd718d4b98ed8a15591eeb068c8229e2b4a3cc0
DoEggsCostMore <>
2025-01-27 10:19:02 -0600

    Add schedule for reddit bot mail

--- routes/console.php
+++ routes/console.php
@@ -5,6 +5,7 @@ use App\Console\Commands\GetData;
 use App\Console\Commands\GetPrices;
 use App\Console\Commands\GetPricesEia;
 use App\Console\Commands\GetPricesKroger;
+use App\Console\Commands\ProcessRedditMail;
 use App\Console\Commands\RedditBot;
 use App\Console\Commands\RunRedditBot;
 use App\Console\Commands\WarmCache;
@@ -12,5 +13,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
+// Data ingest
 Schedule::command(GetData::class)->weekly('18:00');
 Schedule::command(GenerateSitemap::class, ['storage' => 'public'])->weekly('20:00');
+
+// Bot things
+Schedule::command(ProcessRedditMail::class)->everyFiveMinutes();
