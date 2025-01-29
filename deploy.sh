2febee87c70a2baface10e3cb5bb7cfad9f5fe6b
DoEggsCostMore <>
2025-01-28 21:08:40 -0600

    Restart workers on deploy

--- deploy.sh
+++ deploy.sh
@@ -14,3 +14,7 @@ if [ "$1" = "export" ]; then
     # php artisan export
     php artisan app:generate-sitemap public
 fi
+
+# Restart all the workers
+echo "Restarting Reddit Comment Worker"
+pkill -f "reddit-comments.py"
