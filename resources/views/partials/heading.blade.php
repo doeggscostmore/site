ed1d9a2dcdf711622a0a934376395ba76e16b73d
DoEggsCostMore <>
2025-02-03 21:21:08 -0600

    Update product page layout to remove image

--- resources/views/partials/heading.blade.php
+++ resources/views/partials/heading.blade.php
@@ -5,23 +5,29 @@
     <div class="container overall down">
     @endif
         <div class="row align-items-center">
-            <div class="col col-lg-6">
+            <div class="col-12 col-lg-6">
                 <a href="/" class="back-link"><i class="fa fa-arrow-left"></i>Home</a>
                 <h1>{{ ucwords($category->verb) }} {{ ucwords($category->name) }} Cost More?</h1>
                 @if ($data->isUp)
                     <h2>Yes.</h2>
+                @else
+                    <h2>No.</h2>
+                @endif
+            </div>
+            <div class="col-12 col-lg-6">
+                @if ($data->isUp)
                     <span class="tagline">
                         The prices of {{ $category->name }} have gone up <b>{{
                         abs(round($data->change, 1)) }}%</b> since {{ $data->start->format('F, Y') }}.
                     </span>
                 @else
-                    <h2>No.</h2>
                     <span class="tagline">
                         The prices of {{ $category->name }} have gone down <b>{{
                         abs(round($data->change, 1)) }}%</b> since {{ $data->start->format('F, Y') }}
                     </span>
                 @endif
-                
+            </div>
+            <div class="col-12 text-center">
                 <ul class="nav">
                     <li class="nav-item">
                         <a class="nav-link" href="#past-data">Past Data</a>
@@ -36,13 +42,6 @@
                         <a href="#about" class="nav-link">About</a>
                     </li>
                 </ul>
-            </div>
-            <div class="col col-lg-6 d-none d-lg-block">
-                @if ($data->isUp)
-                <img id="picture-bad" alt="an ai-generated image of unhappy food" />
-                @else
-                <img id="picture-good" alt="an ai-generated image of happy food" />
-                @endif
             </div>
         </div>
     </div>
